90882c5e4a6f4ff741efade9f67106974b69760d
Ed Bobak <ebobak@umich.edu>
2022-10-08 16:06:58 -0400

    Add shortcuts for the other three options serial shows.  More info for the future use.

--- main.m
+++ main.m
@@ -35,13 +35,27 @@ int main(int argc, char *argv[])
         }
         shard = (shard % 100) + 1; // add one, so we go 1-100 not 0-99
 
+        // Shortcut -s mode
+        if (argc == 2 && strcmp(argv[1], "-s") == 0) {
+            getSerialNumber(machPort, serial);
+            printf("%s\n", serial);
+            exit(0);
+        }
+		
         // Shortcut -m mode
         if (argc == 2 && strcmp(argv[1], "-m") == 0) {
             getModelName(machPort, model);
             printf("%s\n", model);
             exit(0);
         }
-
+		
+        // MAC addresses here
+        if (argc == 2 && strcasecmp(argv[1], "--mac") == 0) {
+			getInterface(machPort, macAddress);
+            printf("%02x:%02x:%02x:%02x:%02x:%02x\n", macAddress[0], macAddress[1], macAddress[2], macAddress[3], macAddress[4], macAddress[5]);
+            exit(0);
+        }
+		
         // Shard only
         if (argc == 2 && strcasecmp(argv[1], "--shard") == 0) {
             printf("%d\n", shard);
@@ -53,7 +67,14 @@ int main(int argc, char *argv[])
             printf("<result>%d</result>\n", shard);
             exit(0);
         }
-
+		
+        // UUID only
+        if (argc == 2 && strcasecmp(argv[1], "--uuid) == 0) {
+			getDeviceUuid(machPort, uuid);ial
+            printf("%s\n", uuid);
+            exit(0);
+        }
+		
         // Otherwise: all the numbers
 		printf("%s\n", serial);
 		printf("%s\n", model);
